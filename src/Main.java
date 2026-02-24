4ac2a2daabe87c80c4bddf377e0d847abb413b0b
Bing639-star <2519670741@qq.com>
2026-02-24 23:59:12 +0800

    Java练习

---
+++ src/Main.java
@@ -0,0 +1,100 @@
+//TIP 要<b>运行</b>代码，请按 <shortcut actionId="Run"/> 或
+// 点击装订区域中的 <icon src="AllIcons.Actions.Execute"/> 图标。
+//public class Main {
+//    public static void main(String[] args) {
+        //TIP 当文本光标位于高亮显示的文本处时按 <shortcut actionId="ShowIntentionActions"/>
+        // 查看 IntelliJ IDEA 建议如何修正。
+//        System.out.printf("Hello and welcome!");
+//
+//        for (int i = 1; i <= 5; i++) {
+//            //TIP 按 <shortcut actionId="Debug"/> 开始调试代码。我们已经设置了一个 <icon src="AllIcons.Debugger.Db_set_breakpoint"/> 断点
+//            // 但您始终可以通过按 <shortcut actionId="ToggleLineBreakpoint"/> 添加更多断点。
+//            System.out.println("i = " + i);
+//        }
+
+
+
+
+//        //var 关键字，可以在局部变量声明时自动推断变量类型，从而简化代码。
+//        int a = 10;
+//         //如果需要改成其他类型，前面的int也得跟着改
+//        var a = 10;
+//       // 用var关键字来根据后面的结果自动识别
+
+
+
+
+     //位运算符包括：& | ^ ~
+//        public static void main(String[] args) {
+//            int a = 9, b = 3;
+//            int c = a & b;    //进行按位与运算
+//            System.out.println(c);
+//        }
+
+//        public static void main(String[] args) {
+//            int a = 9, b = 3;
+//            int c = a | b;
+//            System.out.println(c);
+//    }
+//
+//        public static void main(String[] args) {
+//            int a = 9, b = 3;
+//            int c = a ^ b;
+//            System.out.println(c);
+//    }
+//
+//        public static void main(String[] args) {
+//            byte c = ~127;
+//            System.out.println(c);
+//    }
+//
+//        public static void main(String[] args) {
+//            byte c = 8 >> 2;
+//            System.out.println(c);
+//        }
+//
+//        public static void main(String[] args) {
+//            byte c = -4 >> 1;
+//            System.out.println(c);
+//    }
+//
+////    左移操作<<： 高位直接丢弃，低位补0
+////    右移操作>>： 低位直接丢弃，符号位是什么高位补什么
+//
+//    public static void main(String[] args) {
+//        int c = -1 >> 1;   //正常的右移操作，高位补1，所以说移了还是-1
+//        System.out.println(c);
+//    }
+//
+//    public static void main(String[] args) {
+//        int c = -1 >>> 1;   //无符号右移是三个大于符号连在一起，移动会直接考虑符号位
+//        System.out.println(c);
+//    }
+//
+//    //可以缩写：
+//    public static void main(String[] args) {
+//        int c = -1;
+//        c = c << 2;
+//        System.out.println(c);
+//    }
+//
+//    public static void main(String[] args) {
+//        int c = -1;
+//        c <<= 2;    //直接运算符连上等号即可，跟上面是一样的
+//        System.out.println(c);
+//    }
+
+
+// 优先级	运算符	                         结合性
+//1	( )	                                    从左向右
+//2	~ - + (强制类型转换) ++ --	            从右向左
+//3	* / %	                                从左向右
+//4	+ -	                                    从左向右
+//5	<< >> >>>	                             从左向右
+//6	&	                                    从左向右
+//7	^	                                    从左向右
+//8	|	                                    从左向右
+//9	= += -= *= /= %= &= |= ^= <<= >>= >>>=	从右向左
+
+}
+}
